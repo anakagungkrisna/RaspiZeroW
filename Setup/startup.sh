6cd9c5f09314a922a034bbc340a78de06285de01
anakagungkrisna <mamoforce55@gmail.com>
2022-10-09 09:31:01 +0100

    add startup.sh and modify setup notes

---
+++ Setup/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "connecting to internet..."
+sudo systemctl stop NetworkManager
+sudo ifconfig wlan0 up
+wpa_passphrase "Krisna" ibutuden69 | sudo tee /etc/wpa_supplicant.conf
+sudo killall wpa_supplicant
+sudo wpa_supplicant -B -c /etc/wpa_supplicant.conf -i wlan0
+sudo dhclient wlan0
+ifconfig wlan0
+echo "successfully connected to internet!"
