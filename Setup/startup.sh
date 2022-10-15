14e90ad48d5e794eb875b175215d553898a8072c
anakagungkrisna <krisnakusuma0500@gmail.com>
2022-10-15 02:48:23 +0100

    updating notes

--- Setup/startup.sh
+++ Setup/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 echo "connecting to internet..."
 sudo systemctl stop NetworkManager
+sudo systemctl disable NetworkManager
 sudo ifconfig wlan0 up
 wpa_passphrase "Krisna" ibutuden69 | sudo tee /etc/wpa_supplicant.conf
 sudo killall wpa_supplicant
