f64e6ac28a2bc66649149a9fac53528cc2685483
anakagungkrisna <krisnakusuma0500@gmail.com>
2022-11-17 19:46:54 +0000

    update setup and catkin_ws

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/krisna/RaspiZeroW/catkin_ws/src
+# Build directory: /home/krisna/RaspiZeroW/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("testing_software")
---
+++ catkin_ws/build/testing_software/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/krisna/RaspiZeroW/catkin_ws/src/testing_software
+# Build directory: /home/krisna/RaspiZeroW/catkin_ws/build/testing_software
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
